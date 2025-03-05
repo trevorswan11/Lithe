7103a6f18a8851797a36a293ca5c4d0ab6786f83
Trevor Swan <tcs94@case.edu>
2025-03-04 23:38:56 -0500

    vertex buffer layouts! really cool stuff. next is vertex arrays https://www.youtube.com/watch?v=rkxrw8dNrvI&list=PLlrATfBNZ98dC-V-N3m0Go4deliWHPFwT&index=32

--- Lithe/src/Lithe/Application.cpp
+++ Lithe/src/Lithe/Application.cpp
@@ -10,6 +10,28 @@ namespace Lithe {
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
 	Application* Application::s_Instance = nullptr;
+
+	static GLenum ShaderDataTypeToOpneGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+			case ShaderDataType::Float:		return GL_FLOAT;
+			case ShaderDataType::Float2:	return GL_FLOAT;
+			case ShaderDataType::Float3:	return GL_FLOAT;
+			case ShaderDataType::Float4:	return GL_FLOAT;
+			case ShaderDataType::Mat3:		return GL_FLOAT;
+			case ShaderDataType::Mat4:		return GL_FLOAT;
+			case ShaderDataType::Int:		return GL_INT;
+			case ShaderDataType::Int2:		return GL_INT;
+			case ShaderDataType::Int3:		return GL_INT;
+			case ShaderDataType::Int4:		return GL_INT;
+			case ShaderDataType::Bool:		return GL_BOOL;
+		}
+
+		LI_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		LI_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -27,16 +49,38 @@ namespace Lithe {
 		glBindVertexArray(m_VertexArray);
 
 		// Vertex Buffer
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,	// Bottom-left
-			0.5f, -0.5f, 0.0f,	// Bottom-right
-			0.0f, 0.5f, 0.0f	// Top
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			0.0f, 0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		// Code is deleted immediately after exiting this 
+		{
+			BufferLayout layout = {
+			{ ShaderDataType::Float3, "a_Position" },
+			{ ShaderDataType::Float4, "a_Color" }
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
+		
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, 
+				element.GetComponentCount(), 
+				ShaderDataTypeToOpneGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE, 
+				layout.GetStride(),
+				reinterpret_cast<const void*>(static_cast<uintptr_t>(element.Offset))
+			);
+			index++;
+		}
 		
 		// Index Buffer
 		uint32_t indices[3] = { 0, 1, 2 };
@@ -46,13 +90,16 @@ namespace Lithe {
 		std::string vertexSrc = R"(
 			#version 330 core
 
-			layout(location = 0) in vec3 a_Position;	
+			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 
-			out vec3 v_Position;		
+			out vec3 v_Position;
+			out vec4 v_Color;	
 
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);
 			}
 		)";
@@ -63,10 +110,12 @@ namespace Lithe {
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;				
+			in vec4 v_Color;	
 
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
